8098534420ce0699f84789a89785fa71faa6723a
Dragon1754 <153112995+Dragon1754@users.noreply.github.com>
2023-12-06 23:54:11 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ API_ID = int(os.environ.get("API_ID", "28572833"))
 
 API_HASH = os.environ.get("API_HASH", "f27982dd907796571bfc78844f73eaa2")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQAOnoCSRZ0VHwoIKSly5ApJLw_Ll0glHTg3dbTq9gWCmy74FAkfREpEhnC9WzhyNuTdYvgYvONlvrjjs1JThqJSozJnaIb9vO0MBpUxKX0_oaWaAiBW_aQOSY8lX_DjsOJ8j-ju0Ath9oCin6-_YMu6oXwT4oEhfttJt13yFxfgp73dGncxxtYF7ufqKgrW7LxHEVfy_buLGXQrNlvoeb-iHF3urgFrK8KBvmdTuq5sJX2yXA2zxaRDFO8qDthpr0N5Wm4S9AblltnvR3uOJGrE97SUnJiS6wXJOTuiWVnh-AIdNJlv17RDaTXQ_zu1g4ow4b8bW2trE4X8TB7x0pQFAAAAAXxXym8A")
 
 
 
